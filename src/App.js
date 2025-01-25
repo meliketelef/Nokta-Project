b46e8f1b238c5684d783a4a556e42bb917842e23
meliketelef <meliketelef0935@gmail.com>
2025-01-25 23:19:44 +0300

    İlk commit

--- src/App.js
+++ src/App.js
@@ -1,62 +1,97 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Login from "./screens/Login";
 import Signup from "./screens/Signup";
 import HomePage from "./screens/HomePage";
 import "./App.css";
+import logo from "./images/NoktaLogo.png";
 
 function App() {
   return (
     <Router>
-      <div>
-        <Routes>
-          {/* Login ve Signup rotaları */}
-          <Route path="/" element={<HeaderLayout component={<Login />} />} />
-          <Route
-            path="/signup"
-            element={<HeaderLayout component={<Signup />} />}
-          />
+      <Routes>
+        {/* Login ve Signup sayfaları header ile birlikte görünür */}
+        <Route
+          path="/"
+          element={
+            <HeaderLayout>
+              <Login />
+            </HeaderLayout>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <HeaderLayout>
+              <Signup />
+            </HeaderLayout>
+          }
+        />
 
-          {/* HomePage rotası */}
-          <Route path="/homepage" element={<HomePageLayout />} />
-        </Routes>
-      </div>
+        {/* HomePage sayfası tamamen farklı bir düzen alır */}
+        <Route path="/homepage" element={<HomePage />} />
+      </Routes>
     </Router>
   );
 }
 
-/* Header ile Layout */
-const HeaderLayout = ({ component }) => {
+/* Header ile sarılı bir düzen */
+const HeaderLayout = ({ children }) => {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="logo">Nokta</h1>
-        <div className="toggle-buttons">
-          <button>
-            <a href="/" style={{ textDecoration: "none", color: "white" }}>
+        <div className="logo-container">
+          <img src={logo} alt="Nokta Logo" className="logo" />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginBottom: "20px",
+          }}
+        >
+          <button
+            style={{
+              backgroundColor: "white",
+              color: "#004080",
+              border: "1px solid #004080",
+              padding: "10px 20px",
+              cursor: "pointer",
+              marginRight: "5px",
+            }}
+          >
+            <Link
+              to="/"
+              style={{
+                textDecoration: "none",
+                color: "#004080",
+              }}
+            >
               Login
-            </a>
+            </Link>
           </button>
-          <button>
-            <a
-              href="/signup"
-              style={{ textDecoration: "none", color: "white" }}
+          <button
+            style={{
+              backgroundColor: "white",
+              color: "#004080",
+              border: "1px solid #004080",
+              padding: "10px 20px",
+              cursor: "pointer",
+            }}
+          >
+            <Link
+              to="/signup"
+              style={{
+                textDecoration: "none",
+                color: "#004080",
+              }}
             >
               Signup
-            </a>
+            </Link>
           </button>
         </div>
       </header>
-      {component}
-    </div>
-  );
-};
-
-/* Sadece HomePage Layout */
-const HomePageLayout = () => {
-  return (
-    <div className="home-container">
-      <HomePage />
+      {children}
     </div>
   );
 };
